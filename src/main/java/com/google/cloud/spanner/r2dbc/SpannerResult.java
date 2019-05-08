bf8ac300a129b94d9c365cebda2684947a0a4180
cccz <cccz@google.com>
2019-05-08 16:40:33 -0400

    use zip instead

--- src/main/java/com/google/cloud/spanner/r2dbc/SpannerResult.java
+++ src/main/java/com/google/cloud/spanner/r2dbc/SpannerResult.java
@@ -75,11 +75,8 @@ public class SpannerResult implements Result {
       return Flux.empty();
     }
 
-    return Flux.defer(() -> {
-      ResultSetMetadata resultSetMetadata = this.resultSetMetadata.block();
-      return this.resultRows.map(row -> f
-          .apply(new SpannerRow(row, resultSetMetadata.getRowType()),
-              new SpannerRowMetadata(resultSetMetadata)));
-    });
+    return this.resultRows.zipWith(this.resultSetMetadata).map(tuple -> f
+        .apply(new SpannerRow(tuple.getT1(), tuple.getT2().getRowType()),
+            new SpannerRowMetadata(tuple.getT2())));
   }
 }
