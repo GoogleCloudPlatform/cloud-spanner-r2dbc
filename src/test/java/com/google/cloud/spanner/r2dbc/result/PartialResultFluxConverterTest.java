3c6707680a716ac556dc7b8e983573f95f2cdc0b
cccz <cccz@google.com>
2019-05-20 14:07:28 -0400

    added few more tests

--- src/test/java/com/google/cloud/spanner/r2dbc/result/PartialResultFluxConverterTest.java
+++ src/test/java/com/google/cloud/spanner/r2dbc/result/PartialResultFluxConverterTest.java
@@ -40,39 +40,38 @@ import reactor.core.publisher.Flux;
  */
 public class PartialResultFluxConverterTest {
 
+  // These are the expected final rows' values
+  final Value a1 = Value.newBuilder().setBoolValue(false).build();
+  final Value a2 = Value.newBuilder().setStringValue("abc").build();
+  final Value a3 = Value.newBuilder()
+      .setListValue(ListValue.newBuilder().addAllValues(Arrays.asList(
+          Value.newBuilder().setNumberValue(12).build(),
+          Value.newBuilder().setNumberValue(34).build(),
+          Value.newBuilder().setNumberValue(56).build())).build()).build();
+
+  final Value b1 = Value.newBuilder().setBoolValue(true).build();
+  final Value b2 = Value.newBuilder().setStringValue("xyz").build();
+  final Value b3 = Value.newBuilder()
+      .setListValue(ListValue.newBuilder().addAllValues(Arrays.asList(
+          Value.newBuilder().setNumberValue(78).build(),
+          Value.newBuilder().setNumberValue(910).build(),
+          Value.newBuilder().setNumberValue(1122).build())).build()).build();
+
+  ResultSetMetadata resultSetMetadata = ResultSetMetadata.newBuilder().setRowType(
+      StructType.newBuilder()
+          .addFields(Field.newBuilder().setName("boolField").build())
+          .addFields(Field.newBuilder().setName("stringField").build())
+          .addFields(Field.newBuilder().setName("listField").build())
+          .build()
+  ).build();
+
   @Test
   public void assembleRowsTest() {
-
-    // These are the expected final rows' values
-    final Value a1 = Value.newBuilder().setBoolValue(false).build();
-    final Value a2 = Value.newBuilder().setStringValue("abc").build();
-    final Value a3 = Value.newBuilder()
-        .setListValue(ListValue.newBuilder().addAllValues(Arrays.asList(
-        Value.newBuilder().setNumberValue(12).build(),
-        Value.newBuilder().setNumberValue(34).build(),
-        Value.newBuilder().setNumberValue(56).build())).build()).build();
-
-    final Value b1 = Value.newBuilder().setBoolValue(true).build();
-    final Value b2 = Value.newBuilder().setStringValue("xyz").build();
-    final Value b3 = Value.newBuilder()
-        .setListValue(ListValue.newBuilder().addAllValues(Arrays.asList(
-        Value.newBuilder().setNumberValue(78).build(),
-        Value.newBuilder().setNumberValue(910).build(),
-        Value.newBuilder().setNumberValue(1122).build())).build()).build();
-
-    ResultSetMetadata resultSetMetadata = ResultSetMetadata.newBuilder().setRowType(
-        StructType.newBuilder()
-            .addFields(Field.newBuilder().setName("boolField").build())
-            .addFields(Field.newBuilder().setName("stringField").build())
-            .addFields(Field.newBuilder().setName("listField").build())
-            .build()
-    ).build();
-
     // The values above will be split across several partial result sets.
     PartialResultSet p1 = PartialResultSet.newBuilder().setMetadata(
-        resultSetMetadata
+        this.resultSetMetadata
     ).setChunkedValue(false)
-        .addValues(a1).build();
+        .addValues(this.a1).build();
 
     PartialResultSet p2 = PartialResultSet.newBuilder()
         .addValues(Value.newBuilder().setStringValue("a")).setChunkedValue(true).build();
@@ -117,10 +116,55 @@ public class PartialResultFluxConverterTest {
 
     Flux<PartialResultSet> inputs = Flux.just(p1, p2, p3, p4, p5, p6, p7, p8);
 
+    verifyRows(inputs);
+  }
+
+  @Test
+  public void singleResultSetTest() {
+    PartialResultSet p1 = PartialResultSet.newBuilder().setMetadata(
+        this.resultSetMetadata
+    ).setChunkedValue(false)
+        .addValues(this.a1)
+        .addValues(this.a2)
+        .addValues(this.a3)
+        .addValues(this.b1)
+        .addValues(this.b2)
+        .addValues(this.b3)
+        .build();
+
+    Flux<PartialResultSet> inputs = Flux.just(p1);
+
+    verifyRows(inputs);
+  }
+
+  @Test
+  public void neatRowsResultSetTest() {
+    PartialResultSet p1 = PartialResultSet.newBuilder().setMetadata(
+        this.resultSetMetadata
+    ).setChunkedValue(false)
+        .addValues(this.a1)
+        .addValues(this.a2)
+        .addValues(this.a3)
+        .build();
+
+    PartialResultSet p2 = PartialResultSet.newBuilder().setMetadata(
+        this.resultSetMetadata
+    ).setChunkedValue(false)
+        .addValues(this.b1)
+        .addValues(this.b2)
+        .addValues(this.b3)
+        .build();
+
+    Flux<PartialResultSet> inputs = Flux.just(p1, p2);
+
+    verifyRows(inputs);
+  }
+
+  private void verifyRows(Flux<PartialResultSet> inputs) {
     List<SpannerRow> results = new PartialResultFluxConverter(inputs).toRows().collectList()
         .block();
 
-    List<ColumnMetadata> columnMetadata = resultSetMetadata.getRowType().getFieldsList()
+    List<ColumnMetadata> columnMetadata = this.resultSetMetadata.getRowType().getFieldsList()
         .stream()
         .map(SpannerColumnMetadata::new)
         .collect(Collectors.toList());
@@ -130,7 +174,7 @@ public class PartialResultFluxConverterTest {
     assertThat(
         Objects.areEqual(columnMetadata, results.get(1).getRowMetadata().getColumnMetadatas()));
 
-    assertThat(results.get(0).getValues()).containsExactly(a1, a2, a3);
-    assertThat(results.get(1).getValues()).containsExactly(b1, b2, b3);
+    assertThat(results.get(0).getValues()).containsExactly(this.a1, this.a2, this.a3);
+    assertThat(results.get(1).getValues()).containsExactly(this.b1, this.b2, this.b3);
   }
 }
