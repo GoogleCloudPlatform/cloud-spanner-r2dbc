84129352168b3a45e63de9727b84c7030a82b55b
zhumin8 <zhumin@google.com>
2021-11-10 13:18:48 -0500

    review feedback updates.

--- cloud-spanner-r2dbc-samples/cloud-spanner-r2dbc-sample/src/main/java/com/example/BookExampleApp.java
+++ cloud-spanner-r2dbc-samples/cloud-spanner-r2dbc-sample/src/main/java/com/example/BookExampleApp.java
@@ -27,6 +27,8 @@ import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
 import io.r2dbc.spi.Option;
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
 import io.r2dbc.spi.Statement;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -70,7 +72,7 @@ public class BookExampleApp {
     Mono.from(this.connection.createStatement("CREATE TABLE BOOKS ("
             + "  ID STRING(20) NOT NULL,"
             + "  TITLE STRING(MAX) NOT NULL,"
-            + "  JSONFIELD JSON"
+            + "  EXTRADETAILS JSON"
             + ") PRIMARY KEY (ID)").execute())
         .doOnSuccess(x -> System.out.println("Table creation completed."))
         .block();
@@ -89,34 +91,25 @@ public class BookExampleApp {
         .bind("title", "Book One")
         .add()
         .bind("id", "book2")
-        .bind("title", "Book Two")
-        .add();
-
-    Flux.concat(this.connection.beginTransaction(),
-        Flux.from(statement.execute()).flatMapSequential(r -> Mono.from(r.getRowsUpdated())).then(),
-        this.connection.commitTransaction()
-    ).doOnComplete(() -> System.out.println("Insert books transaction committed."))
-        .blockLast();
+        .bind("title", "Book Two");
 
 
     Statement statement2 = this.connection.createStatement(
             "INSERT BOOKS "
-                    + "(ID, TITLE, JSONFIELD)"
+                    + "(ID, TITLE, EXTRADETAILS)"
                     + " VALUES "
-                    + "(@id, @title, @jsonfield)")
+                    + "(@id, @title, @extradetails)")
             .bind("id", "book3")
             .bind("title", "Book Three")
-            .bind("jsonfield", new JsonWrapper("{\"rating\":9,\"series\":true}"))
-            .add();
+            .bind("extradetails", new JsonWrapper("{\"rating\":9,\"series\":true}"));
 
     Flux.concat(
             this.connection.beginTransaction(),
-            Flux.from(statement2.execute())
+            Flux.concat(statement.execute(), statement2.execute())
                 .flatMapSequential(r -> Mono.from(r.getRowsUpdated()))
                 .then(),
             this.connection.commitTransaction())
-        .doOnComplete(
-            () -> System.out.println("Insert 3rd book with JSON field transaction committed."))
+        .doOnComplete(() -> System.out.println("Insert books transaction committed."))
         .blockLast();
   }
 
@@ -128,20 +121,7 @@ public class BookExampleApp {
         .flatMap(
             spannerResult ->
                 spannerResult.map(
-                    (r, meta) -> {
-                      if (r.get("JSONFIELD", JsonWrapper.class) != null) {
-                        return "Retrieved book: "
-                            + r.get("ID", String.class)
-                            + "; Title: "
-                            + r.get("TITLE", String.class)
-                            + "; Extra Details: "
-                            + r.get("JSONFIELD", JsonWrapper.class).toString();
-                      }
-                      return "Retrieved book: "
-                          + r.get("ID", String.class)
-                          + "; Title: "
-                          + r.get("TITLE", String.class);
-                    }))
+                    (Row r, RowMetadata meta) -> describeBook(r)))
         .doOnNext(System.out::println)
         .collectList()
         .block();
@@ -158,5 +138,21 @@ public class BookExampleApp {
     } catch (Exception e) {
       System.out.println("Table wasn't found, so no action was taken.");
     }
+  }
+
+  /**
+   * Method to describe row from table BOOK.
+   */
+  public String describeBook(Row r) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("Retrieved book: ");
+    stringBuilder.append(r.get("ID", String.class));
+    stringBuilder.append("; Title: ");
+    stringBuilder.append(r.get("TITLE", String.class));
+    if (r.get("EXTRADETAILS", JsonWrapper.class) != null) {
+      stringBuilder.append("; Extra Details: ");
+      stringBuilder.append(r.get("EXTRADETAILS", JsonWrapper.class).toString());
+    }
+    return stringBuilder.toString();
   }
 }
